7a369ea0f5524de87e111763377ad820266c8789
Smitty Penman <smitty.penman@gmail.com>
2023-04-17 15:38:14 -0400

    add hover and account selection to sidebar

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -7,22 +7,18 @@ import Image from "next/image";
 export const UserSidebar = () => {
   // return a vertical bar sticky to the left side of the screen with a round button for the user's avatar and buttons below which are for the user's accounts, also in round buttons
 
-  const { accounts, setActiveAccount, setIsChoosingAccount } =
-    useAccountStore();
-
-  const handleAccountClick = (index: number) => {
-    setActiveAccount(index);
-    setIsChoosingAccount(false);
-  };
-
   return (
     <div className="flex h-full w-16 flex-col bg-stone-800">
-      <div className=" h-full justify-center">
-        <ProfileThumbnail />
+      <div className=" mt-10 flex h-full flex-col gap-6">
+        <div className="">
+          <ProfileThumbnail />
+        </div>
         <div className="">
           <HorizontalBar />
         </div>
-        <AccountThumbnails />
+        <div className="">
+          <AccountThumbnails />
+        </div>
         <div className="">
           <HorizontalBar />
         </div>
@@ -34,6 +30,7 @@ export const UserSidebar = () => {
   );
 };
 
+import loginIcon from "~/assets/images/login.svg";
 const ProfileThumbnail = () => {
   const { user } = useUser();
   return (
@@ -51,8 +48,12 @@ const ProfileThumbnail = () => {
             <div className="inset-left-0 absolute inset-y-0 z-10 h-10  w-10 rounded-md bg-slate-300 opacity-0 hover:border hover:border-stone-300  hover:opacity-30"></div>
           </div>
         ) : (
-          <ThumbnailCircle
-            character={user && user.username ? user.username[0] : "?"}
+          <Image
+            src={loginIcon}
+            alt="User profile image"
+            width={100}
+            height={100}
+            className=" rounded-lg hover:border hover:border-stone-300"
           />
         )}
       </div>
@@ -68,21 +69,52 @@ import {
 } from "~/components/ui/tooltip";
 
 const AccountThumbnails = () => {
-  const { accounts } = useAccountStore();
+  const {
+    account: thisAccount,
+    accounts,
+    setActiveAccount,
+    setIsChoosingAccount,
+  } = useAccountStore();
+
+  const handleAccountClick = (props: { accountID: string }) => {
+    if (thisAccount?.id != props.accountID) {
+      const accountIndex = accounts.findIndex(
+        (account) => account?.id === props.accountID
+      );
+      setActiveAccount(accountIndex);
+      setIsChoosingAccount(false);
+    }
+  };
+
   return (
-    <div className=" flex flex-col items-center gap-y-4 ">
+    <div className=" flex flex-col items-center gap-y-6 ">
       {accounts.map((account, index) => {
-        if (!account) return;
+        if (!account) return null;
         return (
-          <TooltipProvider key={index}>
+          <TooltipProvider
+            key={index}
+            delayDuration={50}
+            disableHoverableContent={true}
+          >
             <Tooltip>
               <TooltipTrigger asChild>
-                <div>
+                <button
+                  onClick={() =>
+                    handleAccountClick({
+                      accountID: account.id,
+                    })
+                  }
+                  className={
+                    account.id === thisAccount?.id
+                      ? " rounded-lg border-2 border-stone-300"
+                      : "border-2 border-transparent"
+                  }
+                >
                   <ThumbnailCircle account={account} />
-                </div>
+                </button>
               </TooltipTrigger>
-              <TooltipContent sideOffset={4}>
-                <p>Select this account</p>
+              <TooltipContent className=" absolute left-10  my-0.5 h-10 border-2 border-transparent">
+                <p className="whitespace-nowrap">{account.name}</p>
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
